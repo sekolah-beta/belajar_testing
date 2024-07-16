b152d5ae035ea9937ba5b0f9d9b28294a4a042ce
Mambaur Roziq Alwi <alwiroziq1998@gmail.com>
2024-07-16 14:44:10 +0700

    chore: update unit and integration test

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -11,9 +11,6 @@ void main() {
 
     await tester.pumpAndSettle(); // Tunggu hingga semua widget selesai diproses
 
-    // Find the counter text widget
-    final counterTextFinder = find.byKey(const ValueKey('counterText'));
-
     // Find the increment and decrement buttons
     final incrementButtonFinder = find.byKey(const ValueKey('incrementButton'));
     final decrementButtonFinder = find.byKey(const ValueKey('decrementButton'));
@@ -21,10 +18,14 @@ void main() {
     // Initial counter value should be 0
     expect(find.text('0'), findsOneWidget);
 
+    await Future.delayed(const Duration(seconds: 2));
+
     // Tap the increment button and verify the counter value increases
     await tester.tap(incrementButtonFinder);
     await tester.pumpAndSettle();
     expect(find.text('1'), findsOneWidget);
+
+    await Future.delayed(const Duration(seconds: 2));
 
     // Tap the decrement button and verify the counter value decreases
     await tester.tap(decrementButtonFinder);
